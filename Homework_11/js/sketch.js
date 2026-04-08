7dbba4077ad71e85e9849054344c2f481c4ea29a
Aj <aj.wemple@umontana.edu>
2026-04-08 17:32:54 -0600

    Added Homework 11

---
+++ Homework_11/js/sketch.js
@@ -0,0 +1,178 @@
+var gameStarted = false;
+var space = 32;
+var esc = 27;
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
+var charX = 250;
+var charY = 250;
+var enemyX = 0;
+var enemyY = 0;
+var enemy2X = 500;
+var enemy2Y = 500;
+var enemySpeed = getRandomArbitrary(5, 10);
+var enemy2Speed = getRandomArbitrary(2, 5);
+var mouseShapeX;
+var mouseShapeY;
+
+
+function keyPressed()
+{ /* Prevents default behavior for space and esc keys */
+    if(keyCode === space || keyCode === esc)
+    {
+        return false;
+    }
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function setup()
+{
+    createCanvas(500,500);
+}
+
+function draw()
+{
+    background(24, 61, 40);
+    drawLevel();
+    gameStartStop();
+    
+    if(gameStarted === false)
+    {
+        text("Press space to start!", 150,220);
+    }
+    else
+    {
+        charMovement();
+        drawCharacter();
+        enemyMovement();
+        drawEnemies();
+        checkExit();
+        fill(51, 255, 0)
+        rect(mouseShapeX,mouseShapeY, 30,30)
+    }
+}
+
+function gameStartStop() 
+{
+    if(keyIsDown(space))
+    {
+        gameStarted = true;
+        var w = 87; 
+        var s = 83;
+        var a = 65;
+        var d = 68;
+    }
+    else if(keyIsDown(esc))
+    {
+        gameStarted = false;
+        var w = null; 
+        var s = null;
+        var a = null;
+        var d = null;
+    }  
+}
+
+function drawLevel()
+{
+    stroke(0);
+    fill(0);
+    rect(0,0,500,8);
+    rect(0,0,8,500);
+    rect(0,492,500, 8);
+    rect(492,0,8,200);
+    rect(492,300, 8, 200);
+    textSize(20);
+    text("EXIT >",420,255);
+}
+
+function charMovement()
+{
+    if(keyIsDown(w)) {
+        charY -= 5;
+    }
+    if(keyIsDown(s)) {
+        charY += 5;
+    }
+    if(keyIsDown(a)) {
+        charX -= 5;
+    }
+    if(keyIsDown(d)) {
+        charX += 5;
+    }
+}
+
+function enemyMovement()
+{
+    if(gameStarted === true) {
+        enemyX += enemySpeed;
+        enemyY += enemySpeed; 
+        enemy2X -= enemy2Speed;
+        enemy2Y -= enemy2Speed;
+    }
+    if(enemyX > width)
+    {
+        enemyX = 0;
+        enemyY = getRandomArbitrary(0,400);
+    }
+    if(enemyX < 0)
+    {
+        enemyX = width;
+    }
+    if(enemyY > height)
+    {
+        enemyY = 0;
+    }
+    if(enemyY < 0)
+    {
+        enemyY = height;
+    }
+
+    if(enemy2X > width) {
+        enemy2X = 0;
+    }
+    if(enemy2X < 0) {
+        enemy2X = width;
+        enemy2Y = getRandomArbitrary(0, height);
+    }
+    if(enemy2Y > height) {
+        enemy2Y = height;
+    }
+    if(enemy2Y < 0) {
+        enemy2Y = height;
+    }
+}
+
+function drawCharacter()
+{
+    stroke(2);
+    fill(252, 186, 3);
+    rect(charX,charY, 20,20);
+}
+
+function drawEnemies()
+{
+    stroke(2);
+    fill(128, 1, 1);
+    circle(enemyX,enemyY,30);
+    fill(71, 38, 11)
+    circle(enemy2X,enemy2Y, 20);
+}
+
+function checkExit()
+{
+    if(charX > width && charY > 180 && charY < 290) {
+        fill(0);
+        textSize(40);
+        text("Woo!", 200, 150);
+    }
+}
+
+function mouseClicked()
+{
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
